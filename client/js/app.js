6f4a01354c65410c34b5024ace5afca0a5dd7064
geoff whitehead <geoff1012@gmail.com>
2016-02-20 16:09:55 +0000

    stable: before altering code to create update on input changed

--- client/js/app.js
+++ client/js/app.js
@@ -1,15 +1,30 @@
 mainApp = angular.module('mainApp', ['ngRoute'])
-.config(function($routeProvider)
-{
-    $routeProvider
-        .when('/',
-        {
-            templateUrl: '/partials/data.html',
-            controller: 'DataController'
-        })
-
-    .otherwise(
+    .config(function($routeProvider)
     {
-        redirectTo: '/'
+        $routeProvider
+            .when('/',
+            {
+                templateUrl: '/partials/about.html',
+                controller: 'DataController'
+            })
+            .when('/about',
+            {
+                templateUrl: '/partials/about.html',
+                controller: 'DataController'
+            })
+            .when('/reference',
+            {
+                templateUrl: '/partials/ref_search.html',
+                controller: 'DataController'
+            })
+            .when('/sequential',
+            {
+                templateUrl: '/partials/seq_search.html',
+                controller: 'DataController'
+            })
+
+        .otherwise(
+        {
+            redirectTo: '/'
+        });
     });
-});
